8e7d7b13528c784176240bce37156862b60af588
Neka <neka.1519@gmail.com>
2021-10-29 10:18:57 +0530

    Initial commit

---
+++ src/com/neka/Main.java
@@ -0,0 +1,28 @@
+package com.neka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Static Keyword
+
+        //objects are created inside heap memory
+        //before creating objects you load the class
+        //it's loaded into "class loader" memory
+s
+        //static is used when it's same for all the objects
+        //static variables goes into class loader
+        //if a variable is static it means all the objects share the same value
+        //you access static var using class
+
+        static block
+        static {
+        ceo = "Larry" - this block will be executed only once for all the obj.
+        }
+
+        //static block will be executed when you load the class -always will be executed first even if it's given after the constructor
+        //constructor will be executed for all objects.
+        //if two static methods are given then it goes in sequence
+        //cannot use non-static variable in static block
+    }*/
+    }
+}
